cbcaf4bee6cdaa23bdcde98ca7934ec0fef14844
RWypasek <robert.gameops@gmail.com>
2026-01-31 17:45:54 +0100

    small changes in spawner

--- Assets/Scripts/Room 3 scripts/BadIdeaSpawner.cs
+++ Assets/Scripts/Room 3 scripts/BadIdeaSpawner.cs
@@ -5,11 +5,11 @@ public class BadIdeaSpawner : MonoBehaviour
 {
     [Header("Spawn Settings")]
     [SerializeField] private Transform[] spawnPoints; // 2 punkty spawnów
-    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private GameObject badThoughtPrefab;
 
     [Header("Timing")]
-    public float minSpawnTime = 0.5f;
-    public float maxSpawnTime = 2f;
+    public float minSpawnTime = 2.5f;
+    public float maxSpawnTime = 7f;
 
     private void Start()
     {
@@ -20,14 +20,14 @@ public class BadIdeaSpawner : MonoBehaviour
     {
         while (true)
         {
-            if (spawnPoints.Length == 0 || ballPrefab == null)
+            if (spawnPoints.Length == 0 || badThoughtPrefab == null)
                 yield break; // brak ustawień → przerywamy
 
             // Losowy punkt spawn
             Transform spawn = spawnPoints[Random.Range(0, spawnPoints.Length)];
 
             // Tworzymy prefab – prefab sam ogarnia target
-            Instantiate(ballPrefab, spawn.position, Quaternion.identity);
+            Instantiate(badThoughtPrefab, spawn.position, Quaternion.identity);
 
             // Losowy czas do następnego spawnu
             float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
