98e925a02ae5575dfdd961064c8d5084b9fac967
RWypasek <robert.gameops@gmail.com>
2026-01-31 16:09:45 +0100

    Added spawn points and bad thought projectile

---
+++ Assets/Scripts/Room 3 scripts/BadIdeaSpawner.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BadIdeaSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _badIdea1;
+    [SerializeField] private GameObject _badIdea2;
+    
+    
+}
